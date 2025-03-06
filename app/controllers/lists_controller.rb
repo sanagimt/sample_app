b52986b85eb01b7eb8cc438b36f376b9964a40b0
sanagimt <180413622+sanagimt@users.noreply.github.com>
2025-03-06 19:55:51 +0000

    [Finish]3-3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,28 +7,37 @@ class ListsController < ApplicationController
   end
 
   def create
-    #データを受け取り新規するためのインスタンス作成
-    list = List.new(list_params) #今回はローカル変数（Viewに不要）
+    #データを受け取り新規保存するためのインスタンス作成
+    list = List.new(list_params) 
+    #今回はローカル変数（Viewに不要）
     #データをデータベースに保存するためのsaveメソッド実行
     list.save
-    #トップ画面へのリダイレクト
-    redirect_to '/top'
+    #保存したデータの詳細画面へのリダイレクト
+    redirect_to list_path(list.id)
   end
 
   def index
-  end
+    #データベース内の保存データを全取得するためのインスタンス
+    @lists = List.all
+    #インスタンス変数（viewファイルで情報表示するため）
+  end 
 
   def show
+    #引数を受け取り、URLの:idのレコードを取得するためのインスタンス
+    @list = List.find(params[:id])
+    #レコード1件のみ取得するため単数形
   end
 
   def edit
   end
 
-  private #ここから下はcontrollerの中でしか呼び出せない（境界線）。データベースにアクセスするアクション、viewに表示するアクション等を下に書くとエラーが出る。
+  private
+  #ここから下はcontrollerの中でしか呼び出せない（境界線）。データベースにアクセスするアクション、viewに表示するアクション等を下に書くとエラーが出る。
+  
   # ストロングパラメータ：マスアサインメント脆弱性を防ぐ仕組み
   # ※マスアサインメント脆弱性：悪意のあるユーザーが改竄したデータを保存してしまうシステムの弱さ。
   def list_params
-    params.requre(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body)
     # params:formからのデータが入るところ
     # require:送られたデータ中からモデル名を指定し（:list）、データを絞り込む
     # permit：絞り込んだデータから、保存を許可するカラムを指定
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   get 'lists/new'
+  get 'top' => 'homes#top'
   post 'lists' => 'lists#create' 
-  #post：データを新規するHTTPメソッド
-  #create：新規作成して保存するアクション
-  get 'lists/index'
-  get 'lists/show'
+    #post：データを新規するHTTPメソッド
+    #create：新規作成して保存するアクション
+  get 'lists' => 'lists#index'
+  get 'lists/:id' => 'lists#show', as: 'list'
+    #投稿データを呼び出すルーティングを追加
+    #:idでURLを指定。アクション内にparams[:id]と記述して投稿データを取得する。
+    #as:～として利用できる
   get 'lists/edit'
   get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
